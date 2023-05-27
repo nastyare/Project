1379e044209867c2feeaaa61cdd9a299b7b81a0d
nastyare <117918700+nastyare@users.noreply.github.com>
2023-05-27 13:38:05 +0700

    created menu

--- Project/main.cs
+++ Project/main.cs
@@ -17,6 +17,52 @@ namespace Project
             };
             TicketFactory EconomyTicket = new EconomyTicketFactory();
             TicketFactory BusinessTicket = new BusinessTicketFactory();
+            
+            //менюшка
+            Console.WriteLine("Выберите тип билета:");
+            Console.WriteLine("1 - Эконом-класс");
+            Console.WriteLine("2 - Бизнес-класс");
+            int ticketType = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Выберите направление:");
+            for (int i = 0; i < directions.Count; ++i)
+            {
+                Console.WriteLine($"{i + 1} - {directions[i]}");
+            }
+            int directionIndex = int.Parse(Console.ReadLine()) - 1;
+            string direction = directions[directionIndex];
+
+            Console.WriteLine("Выберите дату:");
+            List<DateTime> dates = datesByDirection[direction];
+            for (int i = 0; i < dates.Count; ++i)
+            {
+                Console.WriteLine($"{i + 1} - {dates[i]:dd.MM.yyyy}");
+            }
+            int dateIndex = int.Parse(Console.ReadLine()) - 1;
+            DateTime date = dates[dateIndex];
+            
+            Console.WriteLine("Выберите авиакомпанию:");
+            for (int i = 0; i < airlines.Count; ++i)
+            {
+                Console.WriteLine($"{i + 1} - {airlines[i]}");
+            }
+            int airlineIndex = int.Parse(Console.ReadLine()) - 1;
+            string airline = airlines[airlineIndex];
+
+            Ticket ticket;
+            if (ticketType == 1)
+            {
+                ticket = economyTicketFactory.CreateTicket(direction, date, airline);
+            }
+            else if (ticketType == 2)
+            {
+                ticket = businessTicketFactory.CreateTicket(direction, date, airline);
+            }
+            else
+            {
+                Console.WriteLine("Неверный тип билета.");
+                return;
+            }
         }
     }
 }
