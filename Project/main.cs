18f709e4912bddad5f8ed90fc3a6fe3c00cc3d71
nastyare <117918700+nastyare@users.noreply.github.com>
2023-05-27 13:27:10 +0700

    added factory method (#2)
    * added factory method
    
    * added directions, airlines, date and variants of tickets

--- Project/main.cs
+++ Project/main.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Project
 {
@@ -10,7 +6,17 @@ namespace Project
     {
         static void Main(string[] args)
         {
-
+            //направление-авиакомпании-даты-билеты
+            List<string> directions = new List<string>() { "Москва-Дубай", "Москва-Сеул", "Москва-Токио" };
+            List<string> airlines = new List<string>() { "S7 Airlines", "Победа", "Аэрофлот" };
+            Dictionary<string, List<DateTime>> datesByDirection = new Dictionary<string, List<DateTime>>()
+            {
+                { "Москва-Дубай", new List<DateTime>() { new DateTime(2023, 6, 1), new DateTime(2023, 6, 8), new DateTime(2023, 6, 15) } },
+                { "Москва-Сеул", new List<DateTime>() { new DateTime(2023, 6, 2), new DateTime(2023, 6, 9), new DateTime(2023, 6, 16) } },
+                { "Москва-Токио", new List<DateTime>() { new DateTime(2023, 6, 3), new DateTime(2023, 6, 10), new DateTime(2023, 6, 17) } }
+            };
+            TicketFactory EconomyTicket = new EconomyTicketFactory();
+            TicketFactory BusinessTicket = new BusinessTicketFactory();
         }
     }
 }
